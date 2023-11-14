f75c07ec537fbbf71d3af643343d3855d5dddca7
izunya <whdtj5615@naver.com>
2023-11-14 14:32:37 +0900

    hover random color

---
+++ js/hidden.js
@@ -0,0 +1,20 @@
+console.log("Hidden Homepage Opened")
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// 모든 버튼 요소에 대한 이벤트 핸들러 등록
+document.querySelectorAll('.b_list button').forEach(function (button) {
+    button.addEventListener('mouseover', function () {
+        this.style.border = '5px solid ' + getRandomColor();
+    });
+    button.addEventListener('mouseout', function () {
+        this.style.border = '3px solid black';
+    });
+});
